027239db6ac00fc2cb9749ec2a5c81b012c482c3
KAES <majedjoker21@gmail.com>
2026-01-04 14:40:04 +0300

    Add files via upload

---
+++ kase/js/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAFHI7rbtJgXt-CWMAKieQIptDMbI1dz0w",
+    authDomain: "kase-school.firebaseapp.com",
+    projectId: "kase-school",
+    storageBucket: "kase-school.firebasestorage.app",
+    messagingSenderId: "256216809316",
+    appId: "1:256216809316:web:38870ad01b3330d3a64471"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
